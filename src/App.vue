cda89d0af70b7323044068185a7a913d9d9c17ca
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-05-29 14:26:37 +0200

    Added the template for how the app should look

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <script setup>
 
-</script>
+const textareaInputHandler = () => {
+	console.log('Textarea');
+};
+
+const generateNextQuestion = () => {
+	console.log('Click');
+};
+
+</script>	
 
 <template>
-	<p class="text-red-500">Hello World!</p>
+	<main class="w-[70%] mt-8">
+		<header class="mb-8">
+			<h1 class="text-center text-3xl">Randomize Questions</h1>
+		</header>
+
+		<div>
+			<textarea class="w-full p-3 resize-none border-2 border-black rounded focus-visible:border-[3px]" name="questions" id="questions" rows="10"
+						@input="textareaInputHandler"></textarea>
+			
+			<p class="bg-green-200 p-5 rounded" id="result">Randomized Question Goes Here</p>
+			
+			<button class="mt-4 bg-green-200 float-right p-3 rounded border-2 border-green-700 transition-colors ease-in-out duration-200 hover:bg-green-300"
+						@click="generateNextQuestion">
+				Next Question
+			</button>	
+		</div>
+	</main>
 </template>
